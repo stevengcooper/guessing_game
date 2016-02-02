71115e5ca9aca45373f8bb9db6b7378ccce0efe4
Steven Cooper <stevengcooper76@gmail.com>
2016-02-02 13:40:59 -0500

    Forgot to save before last commit.

--- random_guessing_game.rb
+++ random_guessing_game.rb
@@ -1,14 +1,8 @@
 def number
-  number = rand(100)
+  number = rand(1..100)
 end
 
 def input
   puts "I picked a number between 1 & 100. Try and guess it if you dare."
   input = gets.chomp.to_i
 end
-
-game_on = true
-
-puts input
-
-puts number
