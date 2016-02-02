f56d303961aae437e2ba9081e93c7cbd6a667f9c
Steven Cooper <stevengcooper76@gmail.com>
2016-02-02 16:34:08 -0500

    all elements of the program are working.

--- guessing_game.rb
+++ guessing_game.rb
@@ -4,40 +4,24 @@ end
 
 def input
   target = random_number
-  puts "HINT: The number is #{target}"
   array = []
   5.times do
     puts "I've choosen a number between 1 & 100. You have 5 guesses to get it right."
-
     number = gets.chomp.to_i
-    if number == target
+    puts array
+    if array.include?(number)
+      puts "#{number} didn't work the first time. Way to waste a guess, genius."
+    elsif number == target
       puts "You won!"
       break
+    elsif number < target
+      puts "You're guess is too low."
+      array << number
+    elsif number > target
+      puts "You're guess is too high."
+      array << number
     end
   end
 end
 
 input
-
-# n = random_number
-# array = []
-# 5.times do
-#   puts "I've choosen a number between 1 & 100. You have 5 guesses to get it right."
-#   array << gets.chomp.to_i
-# end
-
-  # array.each do |number|
-  #   until number == n
-  #     if number < n
-  #       puts "Your guess is too low."
-  #     elsif number > n
-  #       puts "Your guess is too high."
-  #     elsif array.include (n)
-  #       puts "It didn't work last time. Way to waste a guess, genuis."
-  #     elsif number == n
-  #       puts "Winner, Winner, Chicken Dinner."
-  #     else
-  #       puts "You did something I didn't think of."
-  #     end
-  #   end
-  # end
